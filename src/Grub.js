89a81e9529408084bcb586c3356725f8e7eec892
Ivana <ivana.he.lili@gmail.com>
2017-04-09 17:27:29 -0700

    "completed"

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,14 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
+// pseudoclassical style
+Grub.prototype.eat = function (value){
+  //Grub.call(this, value);
+  this.eat = function(){
+
+  }; 
+}
+
 
